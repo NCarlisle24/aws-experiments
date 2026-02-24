cb603c73f6fe865893e37e975529292ce519a973
NCarlisle24 <lorenzonolan12983@gmail.com>
2026-02-24 13:04:38 -0700

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+    return (
+        <div className="font-bold underline">
+            hello world!
+        </div>
+    );
+}
